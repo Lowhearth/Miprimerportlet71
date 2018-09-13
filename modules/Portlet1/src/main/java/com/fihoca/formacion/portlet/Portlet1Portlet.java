98911ffe31291644c3ea1ca1d007db7d7dce7046
Lowhearth <hearthisinyourmind@gmail.com>
2018-09-13 12:43:11 +0200

    Fixed indentation

--- modules/Portlet1/src/main/java/com/fihoca/formacion/portlet/Portlet1Portlet.java
+++ modules/Portlet1/src/main/java/com/fihoca/formacion/portlet/Portlet1Portlet.java
@@ -28,63 +28,55 @@ import org.osgi.service.component.annotations.Component;
 /**
  * @author User
  */
-@Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=Fihoca",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.name=" + Portlet1PortletKeys.Portlet1,
+@Component(immediate = true, property = { "com.liferay.portlet.display-category=Fihoca",
+		"com.liferay.portlet.instanceable=true", "javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp", "javax.portlet.name=" + Portlet1PortletKeys.Portlet1,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
-)
+		"javax.portlet.security-role-ref=power-user,user" }, service = Portlet.class)
 public class Portlet1Portlet extends MVCPortlet {
-	
-	private static Vector<Hashtable<String, String>> FACTURAS= new Vector<Hashtable<String, String>>();
-	
+
+	private static Vector<Hashtable<String, String>> FACTURAS = new Vector<Hashtable<String, String>>();
+
 	static {
-		Hashtable<String,String> facturaIbm = new Hashtable<String, String>();
+		Hashtable<String, String> facturaIbm = new Hashtable<String, String>();
 		facturaIbm.put("num", "2018/01");
 		facturaIbm.put("cliente", "IBM");
 		facturaIbm.put("importe", "18.000");
 		FACTURAS.add(facturaIbm);
-		
-		Hashtable<String,String> facturaGoogle = new Hashtable<String, String>();
+
+		Hashtable<String, String> facturaGoogle = new Hashtable<String, String>();
 		facturaGoogle.put("num", "2018/02");
 		facturaGoogle.put("cliente", "Google");
 		facturaGoogle.put("importe", "28.000");
 		FACTURAS.add(facturaGoogle);
-		
-		Hashtable<String,String> facturaNetflix = new Hashtable<String, String>();
+
+		Hashtable<String, String> facturaNetflix = new Hashtable<String, String>();
 		facturaNetflix.put("num", "2018/03");
 		facturaNetflix.put("cliente", "Netflix");
 		facturaNetflix.put("importe", "12.000");
 		FACTURAS.add(facturaNetflix);
-		
+
 	}
 
 	@Override
 	public void serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse)
 			throws IOException, PortletException {
-		
+
 		resourceResponse.setContentType("application/csv");
 		resourceResponse.addProperty(HttpHeaders.CONTENT_DISPOSITION, "attachment: filename=listado.csv");
-		
-		try(OutputStreamWriter osw = new OutputStreamWriter(resourceResponse.getPortletOutputStream()); CSVPrinter printer = new CSVPrinter(osw, CSVFormat.DEFAULT.withHeader("num", "cliente", "importe"))){
+
+		try (OutputStreamWriter osw = new OutputStreamWriter(resourceResponse.getPortletOutputStream());
+				CSVPrinter printer = new CSVPrinter(osw, CSVFormat.DEFAULT.withHeader("num", "cliente", "importe"))) {
 			for (Hashtable<String, String> factura : FACTURAS) {
 				printer.printRecord(factura.get("num"), factura.get("cliente"), factura.get("importe"));
-			}	
+			}
 			printer.flush();
-		}	catch(UnsupportedEncodingException e) {
+		} catch (UnsupportedEncodingException e) {
 			System.out.println(e.getMessage());
 			throw new IOException("Error", e);
-			
+
 		}
-	
-	
+
 		super.serveResource(resourceRequest, resourceResponse);
 	}
 
@@ -92,72 +84,63 @@ public class Portlet1Portlet extends MVCPortlet {
 	public void render(RenderRequest renderRequest, RenderResponse renderResponse)
 			throws IOException, PortletException {
 		String jspPage;
-		
-		if((jspPage = renderRequest.getParameter("jspPage")) != null) {
+
+		if ((jspPage = renderRequest.getParameter("jspPage")) != null) {
 			if (jspPage.equals("/lista.jsp")) {
 				renderRequest.setAttribute("facturas", FACTURAS);
 			}
 			if (jspPage.equals("/edit.jsp")) {
 				String num = renderRequest.getParameter("num");
-				String cliente = renderRequest.getParameter("cliente"); 
+				String cliente = renderRequest.getParameter("cliente");
 				String importe = renderRequest.getParameter("importe");
-				Hashtable<String, String> FACTURA= new Hashtable<String,String>(); ;
+				Hashtable<String, String> FACTURA = new Hashtable<String, String>();
 				FACTURA.put("num", num);
 				FACTURA.put("cliente", cliente);
 				FACTURA.put("importe", importe);
-				
+
 				renderRequest.setAttribute("factura", FACTURA);
 			}
 		}
-		
+
 		super.render(renderRequest, renderResponse);
 	}
+
 	public void altaFactura(ActionRequest actionRequest, ActionResponse actionResponse)
-		throws IOException, PortletException{
-		
+			throws IOException, PortletException {
+
 		String num = actionRequest.getParameter("num");
-		String cliente = actionRequest.getParameter("cliente"); 
+		String cliente = actionRequest.getParameter("cliente");
 		String importe = actionRequest.getParameter("importe");
-		
-		Hashtable<String, String> facturaNueva = new Hashtable <String, String>();
+
+		Hashtable<String, String> facturaNueva = new Hashtable<String, String>();
 		facturaNueva.put("num", num);
 		facturaNueva.put("cliente", cliente);
 		facturaNueva.put("importe", importe);
 		FACTURAS.add(facturaNueva);
-		
+
 	}
-	
+
 	public void editarFactura(ActionRequest actionRequest, ActionResponse actionResponse)
-			throws IOException, PortletException{
+			throws IOException, PortletException {
+
+		String num = actionRequest.getParameter("num").replaceAll(" ", "");
+		String cliente = actionRequest.getParameter("cliente").replaceAll(" ", "");
+		String importe = actionRequest.getParameter("importe").replaceAll(" ", "");
+
+		this.FACTURAS = FACTURAS.stream().map(f -> {
 			
-			String num = actionRequest.getParameter("num").replaceAll(" ","");
-			String cliente = actionRequest.getParameter("cliente").replaceAll(" ",""); 
-			String importe = actionRequest.getParameter("importe").replaceAll(" ","");
-		
+			if (f.get("num").equals(num)) {
+				Hashtable<String, String> facturaNueva = new Hashtable<String, String>();
+				facturaNueva.put("num", num);
+				facturaNueva.put("cliente", cliente);
+				facturaNueva.put("importe", importe);
+				System.out.println(facturaNueva);
+				return facturaNueva;
+			} else 
+				return f;
 			
-			this.FACTURAS = FACTURAS.stream().map(f -> {
-				System.out.println(num);
-				System.out.println(f.get("num"));
-				System.out.println(f.get("num").equals(num));
-			 if(f.get("num").equals(num)) {
-				 System.out.println("editing");
-				 Hashtable<String, String> facturaNueva = new Hashtable <String, String>();
-				 facturaNueva.put("num", num);
-				 facturaNueva.put("cliente", cliente);
-				 facturaNueva.put("importe", importe);	 
-				 System.out.println(facturaNueva);
-				 return facturaNueva;
-			 }else {
-				 return f;
-			 }
-			 }
-			  ).collect(Collectors.toCollection(Vector::new));
-			
-			
-		}
-	
-	
-	
-	
+		})								.collect(Collectors.toCollection(Vector::new));
+
+	}
+
 }
-	
